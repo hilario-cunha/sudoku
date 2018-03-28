df10b939a6dcb1ad6ed6c6965a0fdc84dd59c715
Hilário Cunha <hilario.cunha@gmail.com>
2018-03-28 22:58:35 +0100

    Added .gitignore + Created Console and Core projects

---
+++ Sudoku.ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Sudoku.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var board = new Sudoku.Core.Board();
+            var result1 = board.AddNewNumberIfValid(0,0, 1);
+            var result2 = board.AddNewNumberIfValid(0,1, 1);
+            var result3 = board.AddNewNumberIfValid(1,1, 1);
+ 
+        }
+    }
+}
---
+++ Sudoku.Core/Board.cs
@@ -0,0 +1,38 @@
+namespace Sudoku.Core
+{
+    public class Board
+    {
+        byte[] data;
+        byte rowLength = 9;
+        byte columnLength = 9;
+        public Board()
+        {
+            data = new byte[columnLength*rowLength];
+        }
+
+        public bool AddNewNumberIfValid(byte x, byte y, byte number)
+        {
+            if(IsNewNumberValid(x, y, number))
+            {
+                data[x+(y * rowLength)] = number;
+                return true;
+            }    
+            return false;
+        }
+
+        public bool IsNewNumberValid(byte x, byte y, byte number)
+        {
+            return IsRowValid((byte)(y * rowLength), number); 
+        }
+
+        bool IsRowValid(byte rowBegin, byte number)
+        {
+            for(int i = rowBegin; i < rowBegin + rowLength; i++)
+            {
+                if(data[i] == number) return false;
+            }
+
+            return true;
+        }
+    }
+}
